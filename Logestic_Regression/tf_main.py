6efa50b651356997731c5d375a4139fe579b647b
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-08-26 14:20:08 +0545

    [Deleted] files

--- Logestic_Regression/tf_main.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-tf_model = pickle.load(open('./Logestic_Regression/tf_model.sav','rb'))
-tf_vectorizer = pickle.load(open('./Logestic_Regression/tf_vectorizer.sav','rb'))
-text = input('Enter a text: ')
-x_transform = tf_vectorizer.transform([text])
-
-print(text,':','pos' if tf_model.predict(x_transform)[0] ==1 else 'neg')
--- SVM/main.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-model = pickle.load(open('./SVM/model.sav','rb'))
-vectorizer = pickle.load(open('./SVM/vectorizer.sav','rb'))
-
-text = input('Enter your review: ')
-vectorize_text = vectorizer.transform([text])
-out = model.predict(vectorize_text)
-print(text,":","pos" if out ==1 else "neg")
