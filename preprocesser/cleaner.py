1c4bb04870296fb75cac8c0897b677ad7b1f03eb
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-08-27 09:23:29 +0545

    [Updated] cleaning

--- preprocesser/cleaner.py
+++ preprocesser/cleaner.py
@@ -29,26 +29,32 @@ def cleaner(text_arg:"String"):
     processed_string = soup.get_text()
     text_arg = re.sub('\[[^]]*\]', '', processed_string)
     
-    # remove contractions . eg: I've : I have
-    processed_string = cn.fix(processed_string)
-    # to lower
-    processed_string = processed_string.lower()
-
-    # remove letter repetation
-    processed_string = re.sub(r'(.)\1+',r'\1\1',processed_string)
-
-   
-    # remove punctuation
-    processed_string = ''.join(ch for ch in processed_string if ch not in string.punctuation)
-  
-    # convert emoji to text
-    processed_string = emoji.demojize(processed_string)
-    
     # remove URLS
     processed_string = re.sub('http[s]?://\S+', '', processed_string)
 
     # remove hashtags
     processed_string = re.sub('#+',"",processed_string)
+
+    # convert emoji to text
+    processed_string = emoji.demojize(processed_string)
+
+    # remove contractions . eg: I've : I have
+    processed_string = cn.fix(processed_string)
+
+    # remove special characters
+    processed_string = re.sub(r'\W',' ',processed_string)
+
+    # to lower
+    processed_string = processed_string.lower()
+
+    # remove all single characters
+    processed_string = re.sub(r'\s+[a-zA-Z]\s+',' ',processed_string)
+
+    # remove letter repetation
+    processed_string = re.sub(r'(.)\1+',r'\1\1',processed_string)
+
+    # remove punctuation
+    processed_string = ''.join(ch for ch in processed_string if ch not in string.punctuation)
 
     #replace username
     replace_username_with = "user"
@@ -56,5 +62,6 @@ def cleaner(text_arg:"String"):
 
     # remove multiple white space
     processed_string = re.sub('(\s+)',' ',processed_string)
+
     return processed_string
 
