58736e6d446a40b0d8054539cd2a0644c22d95ff
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-09-05 23:11:50 +0545

    Added GUI

--- gui/app.py
+++ gui/app.py
@@ -1,16 +1,72 @@
+from tkinter import *
+from tkinter import ttk,filedialog
+from tkinter import scrolledtext
+from tkinter import messagebox
+import os
 
-"""
-    GUI tutorial : https://www.mfitzp.com/tutorials/creating-your-first-pyqt-window/
-    date : 2021/7/13
-"""
+window = Tk()
+window.geometry("800x500")
+window.resizable(0,0)
+window.wm_iconbitmap('gui/icon.ico')
+window.title('Sentimental Analysis')
 
-from PyQt5.QtWidgets import QApplication
-import sys
-from MainWindow import MainWindow
+# row 0
+def mic_clicked():
+    print('mic clicked')
 
-app = QApplication([])
+mic_icon = PhotoImage(file='gui/mic-4.png')
+mic_button = Button(image=mic_icon,command=mic_clicked)
+mic_button.grid(column=0,row=0,padx=10,pady=10)
 
-window = MainWindow()
-window.show()
+heading = Label(text='Sentiment Analysis\n What it does: Detecting the intentions behind text response',font=(24),width=72)
+heading.grid(column=1,row=0)
 
-app.exec()
+def folder_clicked():
+    files = filedialog.askopenfilenames()
+    # print(files[0])
+    f = open(files[0], "r")
+    f_list = f.readlines()
+    f_sentence = ''.join(f_list)
+    text_input.insert(END,f_sentence)
+
+folder_icon = PhotoImage(file='gui/folder-6.png')
+folder_button = Button(image=folder_icon,command=folder_clicked)
+folder_button.grid(column=2,row=0,padx=10,pady=10)
+
+# row 1
+text_input = scrolledtext.ScrolledText(width=60,height=5)
+# text_input.insert(END,'Hello rabin, This is an awesome application\nPlease let me know when you are available.')
+# print(text_input.get('1.0',END))
+text_input.grid(row=2,column=1,padx=4,pady=4)
+
+def analyze_clicked():
+    print('analyze clicked')
+
+analyze_button = Button(text='  Analyze ',command=analyze_clicked,pady=20)
+analyze_button.grid(column=2,row=2)
+
+# row 2
+style = ttk.Style()
+style.theme_use('clam')
+
+tree = ttk.Treeview( column=("Algorithm", "Accuracy", "Result"), show='headings', height=5)
+tree.column("# 1", anchor=CENTER)
+tree.heading("# 1", text="Algorithm")
+tree.column("# 2", anchor=CENTER)
+tree.heading("# 2", text="Accuracy")
+tree.column("# 3", anchor=CENTER)
+tree.heading("# 3", text="Result")
+u1 = ('KNN','86%','Best')
+u2 = ('Random Forest','82%','Middle')
+u3 = ('Decision Tree','80%','Good')
+u4 = ('SVC','72%','Fine')
+u5 = ('Naive Bayes','70%','Ok')
+tree.insert('', 'end', text="1", values=u1)
+tree.insert('', 'end', text="1", values=u2)
+tree.insert('', 'end', text="1", values=u3)
+tree.insert('', 'end', text="1", values=u4)
+tree.insert('', 'end', text="1", values=u5)
+
+tree.grid(row=3,column=1,pady=20)
+
+window.mainloop()
---
+++ gui/test.py
@@ -0,0 +1,120 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import messagebox
+
+window = Tk()
+window.geometry("800x600")
+window.resizable(0,0)
+window.wm_iconbitmap('gui/icon.ico')
+window.title('Sentimental Analysis')
+
+
+# Label
+my_label = Label(text='New Label',font=('Arial',24,'bold'))
+my_label.grid(column=0,row=0)
+# update label
+# my_label.config(text='update')
+
+#input Entry()
+input_text = Entry(width=26)
+input_text.grid(column=0,row=1)
+
+# button
+def button_clicked():
+    my_label.config(text=input_text.get()) # update mylabel from input Entry()
+
+fred = Button(text='Submit',command=button_clicked,bg='skyblue',fg='black')
+# display in screen
+#1 fred.pack()
+#2 fred.place(x=180,y=36)
+fred.grid(column=2,row=1)
+
+# ComboBox
+combo = ttk.Combobox();
+combo["values"] = (1,2,3,4,5,'Text')
+combo.current(3)
+combo.grid(column=1,row=5)
+
+# Text
+text = Text(height=5,width=30)
+text.focus()
+text.insert(END,'This is a multiline text entry')
+print(text.get('1.0',END))
+text.grid(row=2,column=0)
+
+# Spinbox -- number input
+def spinbox_used():
+    print(spinbox.get())
+
+spinbox = Spinbox(from_=0,to=10,width=10,command=spinbox_used)
+spinbox.grid(row=3,column=0)
+
+# Scale
+def scale_used(value):
+    print(value)
+
+scale = Scale(from_=0,to=100,command=scale_used)
+scale.grid(row=2,column=2)
+
+# Checkbutton
+def checkbutton_used():
+    print(check_state.get())
+
+check_state = IntVar() # BoolreanVar() for boolean
+checkbutton = Checkbutton(text='is On?',variable=check_state,command=checkbutton_used)
+checkbutton.grid(row=3,column=1)
+
+# Radio button
+def radio_used():
+    print(radio_state.get())
+
+radio_state = IntVar()
+Radiobutton1 = Radiobutton(text='Option1',value=1,variable=radio_state,command=radio_used)
+Radiobutton2 = Radiobutton(text='Option2',value=2,variable=radio_state,command=radio_used)
+Radiobutton3 = Radiobutton(text='Option3',value=3,variable=radio_state,command=radio_used)
+Radiobutton1.grid(row=4,column=0)
+Radiobutton2.grid(row=4,column=1)
+Radiobutton3.grid(row=4,column=2)
+
+#List Box
+def list_box(event):
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ['apple','pear','orange','papaya','grapes']
+for item in fruits:
+    listbox.insert(fruits.index(item),item)
+listbox.bind("<<ListboxSelect>>",list_box)
+listbox.grid(row=5,column=0)
+
+# Scrolltext
+scroll_text = scrolledtext.ScrolledText(width=40,height=10)
+scroll_text.grid(row=6,padx=4,pady=4)
+
+# MessageBox
+def mseg_box():
+    messagebox.showinfo('Message Title','Message Content')
+
+btn_message = Button(text='Message Box',command=mseg_box)
+btn_message.grid(row=6,column=1)
+
+# Events Mouse Buttons
+def right_fun_bind_btn(event):
+    Label(text ='right button clicked').grid(column=1,row=7)
+
+def left_fun_bind_btn(event):
+    Label(text ='left button clicked').grid(column=1,row=7)
+
+bind_btn = Button(text='text right click')
+bind_btn.bind("<Button-3>",right_fun_bind_btn)
+bind_btn.bind("<Button-1>",left_fun_bind_btn)
+bind_btn.grid(row=7,column=0)
+
+# Add images
+icon = PhotoImage(file='Modules_Python/Tkinter/icon.png')
+img_label = Label(image=icon)
+img_label.grid(row=8,column=0)
+
+
+window.mainloop()
