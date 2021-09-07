967154fda82cd289ace533fcf7af5e793639ecb9
Rabin Phaiju <rabinphaiju15@gmail.com>
2021-09-07 16:17:40 +0545

    Update ui table

--- gui/app.py
+++ gui/app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from typing import Sized
 from stopword import removing_stopwords
 from tkinter import ttk,filedialog
 from tkinter import scrolledtext
@@ -16,7 +17,7 @@ import os
 
 window = Tk()
 window.geometry("826x500")
-window.resizable(0,0)
+# window.resizable(0,0)
 window.wm_iconbitmap('gui/icon.ico')
 window.title('Sentimental Analysis')
 
@@ -97,15 +98,19 @@ def startrecord():
                 while is_recording:
                     data = q.get()
                     if rec.AcceptWaveform(data):
-                        current_text.set('Listening ...')
                         text = ast.literal_eval(rec.FinalResult())['text']
                         global final_text
                         final_text.append(text)
                         text_input.insert(END,final_text[-1]+'\n')
+                        current_text.set('Listening ...')
                     else:
                         text = ast.literal_eval(rec.PartialResult())['partial']
                         text =  [text[i:i+110] for i in range(0, len(text), 50)]
-                        current_text.set('\n'.join(text))
+                        if len(text) ==0:
+                            current_text.set('Listening ...')
+                        else:
+                            current_text.set('\n'.join(text))
+                        
                     if dump_fn is not None:
                         dump_fn.write(data)
                 else:
@@ -125,10 +130,14 @@ def startrecord():
 def mic_clicked():
     global is_recording
     if is_recording:
+        heading2.grid(row=1)
+        heading1.config(text='Sentiment Analysis')
         is_recording = False
         mic_button.config(image=mic_icon)
         second_frame.config(text='Input Text')
     else:
+        heading2.grid(row=0)
+        heading1.config(text='')
         is_recording = True
         mic_button.config(image=mic_rec)
         text_input.delete('0.0', END)
@@ -180,8 +189,9 @@ def analyze_clicked():
     text_input.delete('0.0', END)
     text_input.insert(END,'\n'.join(list_sentence))
     cleaned_text = list_sentence
+    ############################
 
-analyze_button = Button(second_frame,text='  Analyze ',font=('Comic Sans MS',18,'normal'),command=analyze_clicked,borderwidth=1,relief=RAISED ,pady=20,padx=0)
+analyze_button = Button(second_frame,text='  Analyze ',font=('Comic Sans MS',18,'normal'),command=analyze_clicked,borderwidth=1,relief=RAISED ,pady=10)
 analyze_button.grid(column=1,row=0)
 
 # row 2
@@ -193,48 +203,63 @@ third_frame.grid(row=2,column=0,padx=5,pady=8)
 third_notebook = ttk.Notebook(third_frame)
 third_notebook.grid(row=0,column=0)
 
-tree = ttk.Treeview(third_notebook,column=("Algorithm",'Accuracy', "F1 Score", "Result"), show='headings', height=5)
-tree.column("# 1", anchor=CENTER)
+tree = ttk.Treeview(third_notebook,column=("Algorithm",'Accuracy', "F1 Score", "Recall","Precision"), show='headings', height=9)
+tree.column("# 1", anchor=CENTER,width=200)
 tree.heading("#1", text="Algorithm")
-tree.column("# 2", anchor=CENTER)
+tree.column("# 2", anchor=CENTER,width=150)
 tree.heading("#2", text="Accuracy")
-tree.column("# 3", anchor=CENTER)
+tree.column("# 3", anchor=CENTER,width=150)
 tree.heading("#3", text="F1 Score")
-tree.column("# 4", anchor=CENTER)
-tree.heading("#4", text="Result")
-u1 = ('Logestic',0.89,89.5,'Best')
-u2 = ('SVC',0.88,88.2,'Middle')
-u3 = ('Multinomial NB',0.86,86.4,'Good')
-u4 = ('Random Forest',0.85,84.9,'Fine')
-u5 = ('Bernoulli NB',0.84,83.6,'Ok')
-u6 = ('KNN',0.78,79.1,'Ok')
-u7 = ('Gaussain NB',0.76,74.9,'Ok')
-u8 = ('Decision Tree',0.72,71.7,'Ok')
-tree.insert('', 'end', text="1", values=u1)
-tree.insert('', 'end', text="1", values=u2)
-tree.insert('', 'end', text="1", values=u3)
-tree.insert('', 'end', text="1", values=u4)
-tree.insert('', 'end', text="1", values=u5)
-tree.insert('', 'end', text="1", values=u6)
-tree.insert('', 'end', text="1", values=u7)
-tree.insert('', 'end', text="1", values=u8)
+tree.column("# 4", anchor=CENTER,width=150)
+tree.heading("#4", text="Recall")
+tree.column("# 5", anchor=CENTER,width=150)
+tree.heading("#5", text="Precision")
+
+tree.insert('', 'end', text="1", values=('Logestic',0.89,89.5,91.4,88.1))
+tree.insert('', 'end', text="1", values=('SVC',0.88,88.2,89.4,86.9))
+tree.insert('', 'end', text="1", values=('Multinomial NB',0.86,86.4,86.4,86.4))
+tree.insert('', 'end', text="1", values=('Random Forest',0.85,84.9,84.3,85.6))
+tree.insert('', 'end', text="1", values=('Bernoulli NB',0.84,83.6,81.6,86.2))
+tree.insert('', 'end', text="1", values=('KNN',0.78,79.1,84.2,74.5))
+tree.insert('', 'end', text="1", values=('Gaussain NB',0.76,74.9,72.64,77.3))
+tree.insert('', 'end', text="1", values=('Decision Tree',0.72,71.7,71.1,71.9))
 tree.grid(row=0,column=0,pady=10,padx=5)
 
-tree2 = ttk.Treeview(third_notebook,column=("Sentence",'Algorithm', "Sentiment"), show='headings', height=8)
+tree2 = ttk.Treeview(third_notebook,column=("Sentence/Algorithm",'Logestic', "SVC","Multinomial NB","Random Forest","Bernoulli NB","KNN","Gaussain NB","Decision Tree"), show='headings', height=9)
 tree2.column("# 1", anchor=CENTER)
-tree2.heading("#1", text="Sentence")
-tree2.column("# 2", anchor=CENTER)
-tree2.heading("#2", text="Algorithm")
-tree2.column("# 3", anchor=CENTER)
-tree2.heading("#3", text="Sentiment")
-tree2.insert('', 'end', text="1", values=('KNN','86%','Best'))
-tree2.insert('', 'end', text="1", values=('Random Forest','82%','Middle'))
-tree2.insert('', 'end', text="1", values=('Decision Tree','80%','Good'))
-tree2.insert('', 'end', text="1", values=('SVC','72%','Fine'))
-tree2.insert('', 'end', text="1", values=('Naive Bayes','70%','Ok'))
+tree2.heading("#1", text="Sentence/Algorithm")
+tree2.column("# 2", anchor=CENTER,width=38)
+tree2.heading("#2", text="Logestic")
+tree2.column("# 3", anchor=CENTER,width=14)
+tree2.heading("#3", text="SVC")
+tree2.column("# 4", anchor=CENTER,width=78)
+tree2.heading("#4", text="Multinomial NB")
+tree2.column("# 5", anchor=CENTER,width=78)
+tree2.heading("#5", text="Random Forest")
+tree2.column("# 6", anchor=CENTER,width=62)
+tree2.heading("#6", text="Bernoulli NB")
+tree2.column("# 7", anchor=CENTER,width=18)
+tree2.heading("#7", text="KNN")
+tree2.column("# 8", anchor=CENTER,width=60)
+tree2.heading("#8", text="Gaussain NB")
+tree2.column("# 9", anchor=CENTER,width=66)
+tree2.heading("#9", text="Decision Tree")
+
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN1 Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN2 Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN3 Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN4 Random Forest Random Forest Random Forest Random Forest','86%','Best'))
+tree2.insert('', 'end', text="1", values=('KNN5 Random Forest Random Forest Random Forest Random Forest','86%','Best'))
 tree2.grid(row=0,column=0,pady=10,padx=5)
 
-third_notebook.add(tree,text='Result')
 third_notebook.add(tree2,text='Sentiment')
+third_notebook.add(tree,text='Result')
 
 window.mainloop()
